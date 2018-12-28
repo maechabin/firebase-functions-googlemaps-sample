7c641ad474515dc60c32a7153c6c99b5b7281249
maechabin <mitoppo@gmail.com>
2018-12-28 23:41:48 +0900

    コード追加&&修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { createClient, GoogleMapsClient } from '@google/maps';
 import * as Cors from 'cors';
 import { GOOGLE_MAPS_KEY } from '../constants';
 
@@ -11,39 +13,81 @@ const googleMapsClient: GoogleMapsClient = createClient({
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-import * as admin from 'firebase-admin';
 admin.initializeApp();
 
+/**
+ * Geocoding
+ */
 export const geocoding: functions.HttpsFunction = functions.https.onRequest(
   (request: functions.Request, response: functions.Response): void => {
     const address = (request.query && request.query.address) || '東京都';
 
-    googleMapsClient.geocode({
-      address
-    }, function(err, res) {
-      if (!err) {
-        cors(request, response, () => {
-          response.status(200).send({ results: res.json.results });
-        });
-      }
-    });
+    googleMapsClient.geocode(
+      {
+        address,
+      },
+      (err, res) => {
+        if (!err) {
+          cors(
+            request,
+            response,
+            (): void => {
+              response.status(200).send({ results: res.json.results });
+            },
+          );
+        }
+      },
+    );
   },
 );
 
+/**
+ * Directions
+ */
 export const directions: functions.HttpsFunction = functions.https.onRequest(
   (request: functions.Request, response: functions.Response): void => {
     const origin = (request.query && request.query.origin) || '東京都中央区';
-    const destination = (request.query && request.query.destination) || '東京都渋谷区';
+    const destination =
+      (request.query && request.query.destination) || '東京都渋谷区';
 
-    googleMapsClient.directions({
-      origin,
-      destination,
-    }, function(err, res) {
-      if (!err) {
-        cors(request, response, () => {
-          response.status(200).send({ results: res.json });
-        });
-      }
-    });
+    googleMapsClient.directions(
+      {
+        origin,
+        destination,
+      },
+      (err, res) => {
+        if (!err) {
+          cors(request, response, () => {
+            response.status(200).send({ results: res.json });
+          });
+        }
+      },
+    );
+  },
+);
+
+/**
+ * DistanceMatrix
+ */
+export const distanceMatrix: functions.HttpsFunction = functions.https.onRequest(
+  (request: functions.Request, response: functions.Response): void => {
+    const origins = (request.query && request.query.origin) || '東京都中央区';
+    const destinations =
+      (request.query && request.query.destination) || '東京都渋谷区';
+
+    googleMapsClient.distanceMatrix(
+      {
+        units: 'imperial',
+        origins,
+        destinations,
+      },
+      (err, res) => {
+        if (!err) {
+          cors(request, response, () => {
+            response.status(200).send({ results: res.json });
+          });
+        }
+      },
+    );
   },
 );
